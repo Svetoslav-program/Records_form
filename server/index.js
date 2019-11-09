3172a3715b50b65ec91103159eb8e17930000fa8
Svetoslav-program <svetoslav.smirnov2006@gmail.com>
2019-11-09 11:47:32 +0400

    create server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 let express = require("express"),
-  bodyParser = require("dody-parser");
+  bodyParser = require("body-parser");
+let mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb://localhost",function(err, client) {
+  let sandbox = client.db ("sandbox");
+  let humans = sandbox.collection("humans");
+  humans.insert({
+    firstName: request.body.firstName,
+    lastName: request.body.lastName
 
+  }, function(err, result) {
+    console.log(result);
+    client.close();
+  });
+});
 let app = express();
-let urlencodedParser = bodyParser.urlencored({ extended: false });
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.post("/enroll", urlencodedParser, function(request, response) {
 
 });
+app.listen(591);
